f5478b4d91f14fd95d800ff8c768fcd464c037a4
Vito Canadi <vito.canadi@gmail.com>
2018-01-17 18:57:09 +0100

    Simulate reportEvent with counter metric

--- src/Mon/Client.hs
+++ src/Mon/Client.hs
@@ -2,46 +2,30 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module Mon.Client
-   (
-   recordTimer
+   ( recordCounter
+   , recordGauge
+   , recordTimer
+   , reportEvent
+   , Endpoint(..)
+   ,
    ) where
 
 import Data.ByteString (intercalate)
-import Data.Time (UTCTime)
 import Network.Socket (close, defaultProtocol, connect, addrAddress, withSocketsDo, getAddrInfo, addrFamily, SocketType(Datagram), socket)
 import Network.Socket.ByteString (send, recv)
 import Universum hiding (intercalate)
 
 data StatsdMessage = StatsdMessage
-    { smDataType :: DataType
+    { smName :: Name
+    , smValue :: Int
+    , smMetricType :: MetricType
+    , smRate :: Maybe Double
     , smTags :: [(Tag,Maybe Text)]
-    }
-
-data DataType = MetricDT Metric
-              | EventDT Event
-
-data Metric = Metric
-    { mName :: Text
-    , mValue :: Int
-    , mMetricType :: MetricType
-    , mRate :: Maybe Double
     }
 
 data MetricType = Counter | Gauge | Timer
 
-data Event = Event
-    { eTitle :: Text
-    , eText :: Text
-    , eTimestamp :: Maybe UTCTime
-    , eHostname :: Maybe Text
-    , eAggregationKey :: Maybe Text
-    , ePriority :: Maybe Text
-    , eSourceTypeName :: Maybe Text
-    , eAlertType :: Maybe Text
-    }
-
 type Name = Text
-type Title = Text
 type Tag = Text
 type Rate = Double
 
@@ -51,39 +35,16 @@ data Endpoint = Endpoint
     }
 
 encodeStatsdMessage :: StatsdMessage -> ByteString
-encodeStatsdMessage (StatsdMessage smDataType smTags) =
-       encodeDataType smDataType
+encodeStatsdMessage StatsdMessage {..} = intercalate "|" (catMaybes [
+      Just $ encodeUtf8 smName <> ":" <> show smValue
+    , Just $ encodeMetricType smMetricType
+    , ("@"<>) . show <$> smRate ])
     <> encodeTags smTags
-
-encodeDataType :: DataType -> ByteString
-encodeDataType (MetricDT metric) = encodeMetric metric
-encodeDataType (EventDT event) = encodeEvent event
-
-encodeMetric :: Metric -> ByteString
-encodeMetric Metric {..} = intercalate "|" $ catMaybes [
-      Just $ encodeUtf8 mName <> ":" <> show mValue
-    , Just $ encodeMetricType mMetricType
-    , ("@"<>) . show <$> mRate ]
 
 encodeMetricType :: MetricType -> ByteString
 encodeMetricType Counter = "c"
 encodeMetricType Gauge = "g"
 encodeMetricType Timer = "ms"
-
-encodeEvent :: Event -> ByteString
-encodeEvent Event {..} = intercalate "|" $ catMaybes [
-      Just $ "_e{" <> show (length title) <> "," <> show (length text) <> "}:" <> title
-    , Just $ text
-    , ("d:"<>) . show <$> eTimestamp
-    , ("h:"<>) . encodeUtf8 <$> eHostname
-    , ("k:"<>) . encodeUtf8 <$> eAggregationKey
-    , ("p:"<>) . encodeUtf8 <$> ePriority
-    , ("s:"<>) . encodeUtf8 <$> eSourceTypeName
-    , ("t:"<>) . encodeUtf8 <$> eAlertType ]
-
-    where
-    title = encodeUtf8 eTitle
-    text = encodeUtf8 eText
 
 encodeTags :: [(Tag, Maybe Text)] -> ByteString
 encodeTags tags = if null tags then "" else "|#" <> intercalate ","  tagsBSs
@@ -91,38 +52,22 @@ encodeTags tags = if null tags then "" else "|#" <> intercalate ","  tagsBSs
     tagsBSs = fmap (\(tag,tagValue) -> encodeUtf8 $ tag <> maybe "" (":"<>) tagValue)
                    tags
 
-recordDataType :: Endpoint -> DataType -> [(Tag,Text)] -> IO ()
-recordDataType endpoint dataType tags = sendStatsdUDP endpoint $ StatsdMessage
-    { smDataType = dataType
+recordMetric :: MetricType -> Endpoint -> Name -> Rate -> [(Tag,Text)] -> Int -> IO ()
+recordMetric metricType endpoint name rate tags value = sendStatsdUDP endpoint $ StatsdMessage
+    { smName = name
+    , smValue = value
+    , smMetricType = metricType
+    , smRate = Just rate
     , smTags = fmap (bimap identity $ \v -> if null v then Nothing else Just v) tags
     }
-
-recordMetric :: MetricType -> Endpoint -> Name -> Rate -> [(Tag,Text)] -> Int -> IO ()
-recordMetric metricType endpoint name rate tags value = recordDataType endpoint (MetricDT $ Metric
-    { mName = name
-    , mValue = value
-    , mMetricType = metricType
-    , mRate = Just rate
-    })
-    tags
 
 recordCounter, recordGauge, recordTimer :: Endpoint -> Name -> Rate -> [(Tag,Text)] -> Int -> IO ()
 recordCounter = recordMetric Counter
 recordGauge = recordMetric Gauge
 recordTimer = recordMetric Timer
 
-reportEvent :: Endpoint -> Title -> Text -> [(Tag,Text)] -> IO ()
-reportEvent endpoint title text tags = recordDataType endpoint (EventDT $ Event
-    { eTitle = title
-    , eText = text
-    , eTimestamp = Nothing
-    , eHostname = Nothing
-    , eAggregationKey = Nothing
-    , ePriority = Nothing
-    , eSourceTypeName = Nothing
-    , eAlertType = Nothing
-    })
-    tags
+reportEvent :: Endpoint -> Name -> Rate -> [(Tag,Text)] -> IO ()
+reportEvent endpoint name rate tags = recordMetric Counter endpoint name rate tags 0
 
 sendStatsdUDP :: Endpoint -> StatsdMessage -> IO ()
 sendStatsdUDP endpoint statsdMessage = sendUDP endpoint (encodeStatsdMessage statsdMessage)
