f2daa0b9d1593bcb0fd644aee01ec0aae399440c
Vito Canadi <vito.canadi@gmail.com>
2018-01-18 11:46:08 +0100

    Enable Werror. Replace mon.cabal with package.yaml

--- src/Mon/Network.hs
+++ src/Mon/Network.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Mon.Network
        ( Endpoint (..)
        , sendStatsdUDP
        ) where
 
 import Network.Socket (close, defaultProtocol, connect, addrAddress, withSocketsDo, getAddrInfo, addrFamily, SocketType(Datagram), socket)
-import Network.Socket.ByteString (send, recv)
+import Network.Socket.ByteString (send)
 import Universum
 
 import Mon.Types
@@ -18,9 +20,9 @@ sendStatsdUDP :: Endpoint -> StatsdMessage -> IO ()
 sendStatsdUDP endpoint statsdMessage = sendUDP endpoint (encodeStatsdMessage statsdMessage)
 
 sendUDP :: Endpoint -> ByteString -> IO ()
-sendUDP (Endpoint eHost ePort) msg = withSocketsDo $ do
+sendUDP Endpoint {..} msg = withSocketsDo $ do
     (serveraddr:_) <- getAddrInfo Nothing (Just $ toString eHost) (Just $ toString ePort)
     s <- socket (addrFamily serveraddr) Datagram defaultProtocol
-    connect s (addrAddress serveraddr) >> return s
-    send s msg
+    connect s (addrAddress serveraddr)
+    _ <- send s msg
     close s
