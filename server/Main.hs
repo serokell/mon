7fc64e444e386be368e0d3e453305cc2c2f7110e
Vito Canadi <vito.canadi@gmail.com>
2018-01-18 21:55:10 +0100

    Implement ekg-based server

---
+++ server/Main.hs
@@ -0,0 +1,98 @@
+{-# LANGUAGE RecordWildCards #-}
+
+-- | Local and non-persistent monitoring server.
+--   Listens for UDP statsd messages, parses and stores metrics/events
+--   to ekg store whoose data is diplayed by ekg-wai http server
+
+module Main
+       ( main
+       ) where
+
+import Universum hiding (empty, intercalate)
+
+import Data.Map (empty, insert, member, (!))
+import Data.Text (intercalate)
+import Network.Socket (AddrInfoFlag (AI_PASSIVE), Socket, SocketType (Datagram), addrAddress,
+                       addrFamily, addrFlags, bind, close, defaultHints, defaultProtocol,
+                       getAddrInfo, socket, withSocketsDo)
+import Network.Socket.ByteString (recvFrom)
+import Options.Applicative (Parser, argument, auto, execParser, helper, info, metavar, progDesc)
+import System.Metrics (Store, createCounter, createDistribution, createGauge)
+import qualified System.Metrics.Counter as SMC
+import qualified System.Metrics.Distribution as SMD
+import qualified System.Metrics.Gauge as SMG
+import System.Remote.Monitoring.Wai (forkServer, serverMetricStore)
+
+import Mon.Types (Name, StatsdMessage (..), Tag, decodeStatsdMessage)
+import qualified Mon.Types as MT
+
+data Metric = CounterM !SMC.Counter
+            | GaugeM !SMG.Gauge
+            | DistributionM !SMD.Distribution
+
+addValueToMetric :: Int -> Metric -> IO ()
+addValueToMetric value (CounterM counter) =
+    SMC.add counter $ fromIntegral value
+addValueToMetric value (GaugeM gauge) =
+    SMG.add gauge $ fromIntegral value
+addValueToMetric value (DistributionM distriution) =
+    SMD.add distriution $ fromIntegral value
+
+type StoreMap = Map Name Metric
+
+createMetric :: StatsdMessage -> Store -> IO Metric
+createMetric StatsdMessage {..} store  = case smMetricType of
+    MT.Counter -> CounterM <$> createCounter taggedName store
+    MT.Gauge   -> GaugeM <$> createGauge taggedName store
+    MT.Timer   -> DistributionM <$> createDistribution taggedName store
+  where
+    taggedName = tagName smName smTags
+
+tagName :: Name -> [Tag] -> Text
+tagName name tags = name <> (intercalate ";" $ "" : fmap showTag tags)
+  where
+    showTag :: (Text, Text) -> Text
+    showTag (tag, value) = tag <> if null value then "" else "=" <> value
+
+addMeasurement :: StatsdMessage -> Store -> StoreMap -> IO StoreMap
+addMeasurement statsdMessage@StatsdMessage {..} store storeMap = do
+    newMetric <- if member taggedName storeMap
+                    then return (storeMap ! taggedName)
+                    else createMetric statsdMessage store
+    addValueToMetric smValue newMetric
+    return $ insert taggedName newMetric storeMap
+  where
+    taggedName = tagName smName smTags
+
+
+mainArgsP :: Parser (Int, Int)
+mainArgsP = (,)
+        <$> argument auto (metavar "<statsd_listen_port>")
+        <*> argument auto (metavar "<ekg_http_interface_port>")
+
+-- | Specify statsd UDP listening port
+--   and http wai server port
+main :: IO ()
+main = do
+    (listenPort, waiPort)  <- execParser $  info (helper <*> mainArgsP) $
+                                            progDesc "Local monitoring server"
+    store <- serverMetricStore <$> forkServer "127.0.0.1" waiPort
+    listen listenPort store
+
+listen :: Int -> Store -> IO ()
+listen port store = withSocketsDo $ do
+    (serveraddr:_) <- getAddrInfo
+                      (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
+                      Nothing (Just $ show port)
+    sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
+    bind sock (addrAddress serveraddr)
+    handler sock empty
+    close sock
+  where
+    handler :: Socket -> StoreMap -> IO ()
+    handler sock storeMap = do
+        (msg,_) <- recvFrom sock 1024
+        let statsdMessage = decodeStatsdMessage msg
+        newStoreMap <- addMeasurement statsdMessage store storeMap
+        print statsdMessage
+        handler sock newStoreMap
--- src/Mon/Types.hs
+++ src/Mon/Types.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE RecordWildCards #-}
 
--- | StatsdMessage is a representation of a statsd udp message.
+-- | StatsdMessage is a representation of a statsd udp message
+--   This module also contains functions for parsing/encoding
+--   StatsdMessage from/to ByteString in which fromat it is received/sent
+--   by Server/Client
+
 module Mon.Types
        ( StatsdMessage (..)
        , MetricType (..)
@@ -8,11 +12,16 @@ module Mon.Types
        , Tag
        , Rate
        , encodeStatsdMessage
+       , decodeStatsdMessage
        ) where
 
-import Universum hiding (intercalate)
+import Universum hiding (intercalate, takeWhile)
 
+import Data.Attoparsec.ByteString (parseOnly)
+import Data.Attoparsec.ByteString.Char8 (Parser, char, decimal, double, sepBy, string, takeWhile,
+                                         (<?>))
 import Data.ByteString (intercalate)
+
 
 data StatsdMessage = StatsdMessage
     { smName       :: !Name
@@ -47,3 +56,51 @@ encodeTags []   = Nothing
 encodeTags tags = Just $ intercalate "," (map encodeTag tags)
   where
     encodeTag (tag, val) = encodeUtf8 $ tag <> if null val then "" else ":" <> val
+
+
+statsdMessageP :: Parser StatsdMessage
+statsdMessageP = flip (<?>) "statsdMessage" $ do
+    name <- takeWhile (/= ':')
+    void colonP
+    value <- decimal
+    void pipeP
+    metricType <- metricTypeP
+    maybeRate <- optional $ pipeP >> atP >> double
+    maybeTags <- optional $ pipeP >> hashP >> tagsP
+    return $ StatsdMessage
+        { smName = decodeUtf8 name
+        , smValue = value
+        , smMetricType = metricType
+        , smRate = maybeRate
+        , smTags = fromMaybe [] maybeTags
+        }
+
+pipeP, colonP, atP, hashP, commaP :: Parser Char
+[pipeP, colonP, atP, hashP, commaP] = char <$> "|:@#,"
+
+tagsP :: Parser [Tag]
+tagsP = tagP `sepBy` commaP <?> "tags"
+
+tagP, tagKeyP, tagKeyValueP :: Parser Tag
+tagP = tagKeyValueP <|> tagKeyP <?> "tag"
+tagKeyP = flip (<?>) "tagKey" $ do
+    key <- takeWhile (/= ',')
+    return (decodeUtf8 key, "")
+
+tagKeyValueP = flip (<?>) "tagKeyValue" $ do
+    tag <- takeWhile ((&&) <$> (/= ':') <*> (/= ','))
+    void colonP
+    tagValue <- takeWhile (/= ',')
+    return (decodeUtf8 tag, decodeUtf8 tagValue)
+
+metricTypeP :: Parser MetricType
+metricTypeP =
+    (string "c"  >> return Counter)   <|>
+    (string "g"  >> return Gauge)     <|>
+    (string "ms" >> return Timer)
+    <?> "metricType"
+
+decodeStatsdMessage :: ByteString -> StatsdMessage
+decodeStatsdMessage bs = case parseOnly statsdMessageP bs of
+    Left err -> error $ toText err
+    Right sm -> sm
