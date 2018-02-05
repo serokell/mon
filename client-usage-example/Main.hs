0bb734062d90b6eff93efd9f30707edbd3dbce61
Vito Canadi <vito.canadi@gmail.com>
2018-02-05 17:07:08 +0100

    Update stack resolver from 9.21 to 10.4

--- client-usage-example/Main.hs
+++ client-usage-example/Main.hs
@@ -14,7 +14,7 @@ import Mon.Network (Endpoint)
 
 mainArgsP :: Parser (Text, Int)
 mainArgsP = (,)
-        <$> fmap toText (strArgument (metavar "STATSD_HOST"))
+        <$> fmap toText (strArgument (metavar "STATSD_HOST") :: Parser String)
         <*> argument auto (metavar "STATSD_PORT")
 
 cliMessage :: String
